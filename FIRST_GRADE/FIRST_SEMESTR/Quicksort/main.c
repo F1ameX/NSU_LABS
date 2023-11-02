aef47558cefa421344495b9f0ebe46665129bb34
F1ameX <106903166+F1ameX@users.noreply.github.com>
2023-11-02 22:53:13 +0700

    Add files via upload

---
+++ FIRST_GRADE/FIRST_SEMESTR/Quicksort/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+
+void swap(int* a, int* b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+
+void quick_sort(int* array, int left, int right)
+{
+
+    if (left < right)
+    {
+        int checker = array[(left + right) / 2], i = left, j = right;
+        int p;
+
+        while (i <= j)
+        {
+            while (array[i] < checker)
+                i++;
+
+            while (array[j] > checker)
+                j--;
+
+            if (i <= j)
+                swap(&array[i++], &array[j--]);
+
+        }
+        quick_sort(array, left, j);
+        quick_sort(array, i, right);
+    }
+}
+
+
+int main()
+{
+    int N, *array;
+    scanf_s("%d", &N);
+    array = (int*)malloc(sizeof(int) * N);
+
+    for (int i = 0; i < N; i++)
+        scanf_s("%d", &array[i]);
+
+    quick_sort(array, 0, N - 1);
+
+    for (int i = 0; i < N; i++)
+        printf("%d ", array[i]);
+
+    free(array);
+    return 0;
+}
