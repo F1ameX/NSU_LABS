23454fdf4db51c79e60cce8732d460aa2be7f6c0
Andrey Ivanov <106903166+F1ameX@users.noreply.github.com>
2024-03-17 18:41:00 +0700

    working prim`s algorithm. Not all tests passed yet

--- FIRST_GRADE/SECOND_SEMESTR/prim_graph_algorithm/main.c
+++ FIRST_GRADE/SECOND_SEMESTR/prim_graph_algorithm/main.c
@@ -1,10 +1,16 @@
-#include "graph.h"
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #define VERTEX_MAX 5000
 #define EDGE_MAX ((vertex_num * (vertex_num + 1)) / 2)
+
+
+typedef struct Edge{
+    int start;
+    int end;
+    int weight;
+}Edge;
 
 
 int error_check(int vertex_num, int edge_num)
@@ -23,20 +29,82 @@ int error_check(int vertex_num, int edge_num)
 }
 
 
+int min_key(int edge_num, int included[], int min_edge[])
+{
+    int min = INT_MAX, min_index;
+    for (int i = 0; i < edge_num; i++)
+    {
+        if (included[i] == 0 && min_edge[i] < min)
+        {
+            min = min_edge[i];
+            min_index = i;
+        }
+    }
+    return min_index;
+}
+
+void prim(int** graph, int edge_num)
+{
+    int len, min_idx, parent[edge_num], min_edge[edge_num], included[edge_num];
+
+    for (int i = 0; i < edge_num; i++)
+    {
+        min_edge[i] = INT_MAX;
+        included[i] = 0;
+    }
+
+    min_edge[0] = 0;
+    parent[0] = -1;
+
+    for (int i = 0; i < edge_num - 1; i++)
+    {
+        min_idx = min_key(edge_num, included, min_edge);
+        included[min_idx] = 1;
+        for (int j = 0; j < edge_num; j++)
+        {
+            if (graph[min_idx][j] && included[j] == 0 && graph[min_idx][j] < min_edge[j])
+            {
+                parent[j] = min_idx;
+                min_edge[j] = graph[min_idx][j];
+            }
+        }
+    }
+
+    len = 0;
+    for (int i = 1; i < edge_num; i++)
+        len += graph[i][parent[i]];
+
+    if (len == 0)
+        puts("no spanning tree");
+    else
+    {
+        for (int i = 1; i < edge_num; i++)
+            if (parent[i] + 1 < i + 1)
+                printf("%d %d\n", parent[i] + 1, i + 1);
+            else
+                printf("%d %d\n", i + 1, parent[i] + 1);
+    }
+}
+
+
 int main()
 {
-    int string_cnt = 0, vertex_num, edge_num, edge_start, edge_end, edge_len;
+    int string_cnt = 0, vertex_num, edge_num, edge_start, edge_end, edge_weight;
     scanf("%d", &vertex_num);
     scanf("%d", &edge_num);
 
     if (error_check(vertex_num, edge_num) == -1)
         return 0;
 
+    int** graph = (int**)malloc(vertex_num * sizeof(int *));
+    for (int i = 0; i < vertex_num; i++)
+        graph[i] = (int *)calloc(vertex_num, sizeof(int));
+
     for (int i = 0; i < edge_num; i++)
     {
-        scanf("%d %d %d", &edge_start, &edge_end, &edge_len);
+        scanf("%d %d %d", &edge_start, &edge_end, &edge_weight);
 
-        if (edge_len < 0 || edge_len > INT_MAX)
+        if (edge_weight < 0 || edge_weight > INT_MAX)
         {
             puts("bad length");
             return 0;
@@ -49,6 +117,9 @@ int main()
             return 0;
         }
 
+        graph[edge_start - 1][edge_end - 1] = edge_weight;
+        graph[edge_end - 1][edge_start - 1] = edge_weight;
+
         string_cnt++;
     }
 
@@ -57,6 +128,11 @@ int main()
         puts("bad number of lines");
         return 0;
     }
+    prim(graph, edge_num);
+
+    for (int i  = 0; i < edge_num; i++)
+        free(graph[i]);
+    free(graph);
 
     return 0;
 }
