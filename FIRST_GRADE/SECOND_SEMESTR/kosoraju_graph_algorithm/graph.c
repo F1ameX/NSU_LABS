ce34da223fd11be3383229367afe9aad6309bda7
Andrey Ivanov <106903166+F1ameX@users.noreply.github.com>
2024-04-02 17:44:43 +0700

    Commented

--- FIRST_GRADE/SECOND_SEMESTR/kosoraju_graph_algorithm/graph.c
+++ FIRST_GRADE/SECOND_SEMESTR/kosoraju_graph_algorithm/graph.c
@@ -5,11 +5,12 @@
 
 void add_edge(Graph* graph, int start, int end)
 {
-    graph->connections[start][end] = 1;
-    graph->transposed_connections[end][start] = 1;
+    graph->edge[start][end] = 1;
+    graph->transposed_edge[end][start] = 1;
 }
 
 
+// DFS function with component handler
 void dfs(Graph* graph, int vertex, int component, int** connections, Stack* stack)
 {
     graph->visited[vertex] = component;
@@ -18,32 +19,35 @@ void dfs(Graph* graph, int vertex, int component, int** connections, Stack* stac
         if (connections[vertex][i] && !graph->visited[i])
             dfs(graph, i, component, connections, stack);
 
+    // Condition to direct(first) DFS
     if (stack != NULL)
         push(stack, vertex);
 }
 
 
+// Free graph memory function
 void free_graph(Graph *graph)
 {
     for (int i = 0; i < graph->vertex_num; i++) {
-        free(graph->connections[i]);
-        free(graph->transposed_connections[i]);
+        free(graph->edge[i]);
+        free(graph->transposed_edge[i]);
     }
 
-    free(graph->connections);
-    free(graph->transposed_connections);
+    free(graph->edge);
+    free(graph->transposed_edge);
     free(graph->visited);
 }
 
 
+// Graph initialization function
 void init_graph(Graph* graph, int vertex_num) {
     graph->vertex_num = vertex_num;
-    graph->connections = (int **)malloc(vertex_num * sizeof(int *));
-    graph->transposed_connections = (int **)malloc(vertex_num * sizeof(int *));
+    graph->edge = (int **)malloc(vertex_num * sizeof(int *));
+    graph->transposed_edge = (int **)malloc(vertex_num * sizeof(int *));
 
     for (int i = 0; i < vertex_num; i++) {
-        graph->connections[i] = (int *)calloc(vertex_num, sizeof(int));
-        graph->transposed_connections[i] = (int *)calloc(vertex_num, sizeof(int));
+        graph->edge[i] = (int *)calloc(vertex_num, sizeof(int));
+        graph->transposed_edge[i] = (int *)calloc(vertex_num, sizeof(int));
     }
 
     graph->visited = (int *)calloc(vertex_num, sizeof(int));
