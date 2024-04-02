d735065d00c1ab6fb1e7ce00384ff6429c88e8b6
Andrey Ivanov <106903166+F1ameX@users.noreply.github.com>
2024-04-02 17:24:00 +0700

    reduced main function, divided into other files

--- FIRST_GRADE/SECOND_SEMESTR/kosoraju_graph_algorithm/main.c
+++ FIRST_GRADE/SECOND_SEMESTR/kosoraju_graph_algorithm/main.c
@@ -1,47 +1,9 @@
 #include "graph.h"
-#include "stack.h"
+#include "kosoraju.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 #define VERTEX_MAX 2000
 #define EDGE_MAX ((VERTEX_MAX * (VERTEX_MAX + 1) / 2))
-
-
-void dfs(Graph* graph, int vertex, int component, int** connections, Stack* stack)
-{
-    graph->visited[vertex] = component;
-
-    for (int i = 0; i < graph->vertex_num; i++)
-        if (connections[vertex][i] && !graph->visited[i])
-            dfs(graph, i, component, connections, stack);
-
-    if (stack != NULL)
-        push(stack, vertex);
-}
-
-
-void kosaraju(Graph *graph)
-{
-    int paint_code = 0;
-    Stack stack;
-    init_stack(&stack, graph->vertex_num);
-
-    for (int vertex = 0; vertex < graph->vertex_num; vertex++)
-        if (!graph->visited[vertex])
-            dfs(graph, vertex, ++paint_code, graph->connections, &stack);
-
-    for (int i = 0; i < graph->vertex_num; i++)
-        graph->visited[i] = 0;
-
-    while (stack.top != -1)
-    {
-        int vertex = pop(&stack);
-        if (!graph->visited[vertex])
-            dfs(graph, vertex, ++paint_code, graph->transposed_connections, NULL);
-    }
-
-    free(stack.data);
-}
 
 
 int error_check(int edge_num, int vertex_num)
@@ -63,8 +25,8 @@ int error_check(int edge_num, int vertex_num)
 
 
 int main() {
-    int component_size, component_num = 1, edge_end, edge_num, edge_start,
-    max_component, vertex_num;
+    int edge_end, edge_num, edge_start,
+    vertex_num;
     Graph graph;
 
     scanf("%d", &vertex_num);
@@ -96,32 +58,7 @@ int main() {
         add_edge(&graph, edge_start - 1, edge_end - 1);
     }
 
-    kosaraju(&graph);
-
-    max_component = 0;
-    for (int i = 0; i < graph.vertex_num; i++)
-        if (graph.visited[i] > max_component)
-            max_component = graph.visited[i];
-
-
-    for (int component = 1; component <= max_component; component++)
-    {
-        component_size = 0;
-
-        for (int i = 0; i < graph.vertex_num; i++)
-            if (graph.visited[i] == component)
-                component_size++;
-
-        if (component_size > 0)
-        {
-            printf("Component %d: ", component_num++);
-            for (int i = 0; i < graph.vertex_num; i++)
-                if (graph.visited[i] == component)
-                    printf("%d ", i + 1);
-            putchar('\n');
-        }
-    }
-
+    kosoraju(&graph);
     free_graph(&graph);
     return 0;
 }
