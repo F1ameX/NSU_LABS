ea7f8426787c7c70f0b4fd063cbff05dcc58ee87
Andrey Ivanov <106903166+F1ameX@users.noreply.github.com>
2024-09-13 19:20:30 +0700

    csv map sort added

--- SECOND_GRADE/FIRST_SEMESTER/lab0/lab_0b/csv_writer.cpp
+++ SECOND_GRADE/FIRST_SEMESTER/lab0/lab_0b/csv_writer.cpp
@@ -1,14 +1,28 @@
 #include "csv_writer.h"
 
-void CSVWriter::write_to_file(const std::string& output_file_name, const std::map<std::string, int>& words_map, int words_quantity) {
-    std::ofstream file_csv(output_file_name);
+void CSVWriter::write_to_file(const std::string& output_file_name, const std::map<std::string, int>& words_map, int words_quantity)
+{
+    std::ofstream file_csv("/Users/andrewf1amex/Programming/lab_0b/" + output_file_name);
 
     if (!file_csv.is_open())
+    {
         std::cerr << "Wrong path or name to the file. Please ensure in right output file name and retry!" << std::endl;
+        return;
+    }
+
+    std::vector<std::tuple<std::string, int, float>> word_freq;
+
+    for (const auto& [word, count] : words_map)
+    {
+        float percent = (static_cast<float>(count) / static_cast<float>(words_quantity)) * 100;
+        word_freq.emplace_back(word, count, percent);
+    }
+
+    std::sort(word_freq.begin(), word_freq.end(), [](const auto& a, const auto& b) {return std::get<2>(a) < std::get<2>(b);});
 
     file_csv << "Слово,Частота,Частота(%)\n";
-    for (const auto& [word, count] : words_map)
-        file_csv << word << ',' << count << ',' << (float(count) / float(words_quantity)) * 100 << '\n';
+    for (const auto& [word, count, percent] : word_freq)
+        file_csv << word << ',' << count << ',' << percent << '\n';
 
     file_csv.close();
 }
