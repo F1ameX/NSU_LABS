466733a80ea7d67381276a5033c743f96bec32c9
Andrey Ivanov <106903166+F1ameX@users.noreply.github.com>
2024-09-17 22:33:49 +0700

    tests added

--- SECOND_GRADE/FIRST_SEMESTER/lab_1a/bit_array_test.cpp
+++ SECOND_GRADE/FIRST_SEMESTER/lab_1a/bit_array_test.cpp
@@ -1,2 +1,28 @@
 #include "bit_array.h"
 #include "gtest/gtest.h"
+
+
+TEST(bitArrayTest, DefaultConstructor)
+{
+    BitArray bitArray;
+
+    ASSERT_EQ(bitArray.size(), 0);
+    ASSERT_TRUE(bitArray.empty());
+}
+
+
+TEST(BitArrayTest, ConstructorWithValue)
+{
+    BitArray bitArray(10, 5);
+    ASSERT_EQ(bitArray.size(), 10);
+    ASSERT_EQ(bitArray.to_string().substr(7), "101");
+}
+
+
+TEST(BitArrayTest, SetAndGetBit)
+{
+    BitArray bitArray(5, 0);
+    bitArray.set(1, true);
+    ASSERT_TRUE(bitArray[1]);
+    ASSERT_FALSE(bitArray[0]);
+}
