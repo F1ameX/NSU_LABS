94394185cdf5051b7ab77be3b3e69cd7943a71f7
Andrey Ivanov <106903166+F1ameX@users.noreply.github.com>
2024-10-19 13:39:35 +0700

    classes connected

--- SECOND_GRADE/FIRST_SEMESTER/lab_2b/main.cpp
+++ SECOND_GRADE/FIRST_SEMESTER/lab_2b/main.cpp
@@ -1,16 +1,26 @@
 #include "console_parser.h"
 #include "file_manager.h"
+#include "game.h"
 
 
 int main(int argc, char* argv[])
 {
     ConsoleParser parser;
     FileManager filestream;
-
+    Game game(20);
+    
     if (!parser.parse(argc, argv))
-        return 0;
-    else
     {
-
+        std::cerr << "Error: Failed to parse command line arguments." << std::endl;
+        return 0;
     }
+    
+    if (!game.prepare_game(parser, filestream))
+    {
+        std::cerr << "Error: Failed to prepare game." << std::endl;
+        return 0;  
+    }
+
+    std::cout << "Game prepared and ready to start!" << std::endl;
+    return 0;  
 }
