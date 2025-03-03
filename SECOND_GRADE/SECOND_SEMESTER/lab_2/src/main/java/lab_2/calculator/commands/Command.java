e832e300e6897946f0ba01dd8c9b56a5dd9c2e52
Andrew Ivanov <106903166+F1ameX@users.noreply.github.com>
2025-03-03 14:05:53 +0700

    explicit indication of exception

--- SECOND_GRADE/SECOND_SEMESTER/lab_2/src/main/java/lab_2/calculator/commands/Command.java
+++ SECOND_GRADE/SECOND_SEMESTER/lab_2/src/main/java/lab_2/calculator/commands/Command.java
@@ -1,7 +1,8 @@
 package lab_2.calculator.commands;
 import lab_2.calculator.context.ExecutionContext;
+import lab_2.calculator.exceptions.CalculatorException;
 import java.util.List;
 
 public interface Command {
-    void execute(ExecutionContext context, List<String> args) throws Exception;
+    void execute(ExecutionContext context, List<String> args) throws CalculatorException;
 }
