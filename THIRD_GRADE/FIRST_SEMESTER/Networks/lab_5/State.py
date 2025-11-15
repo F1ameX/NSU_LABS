14da158d9bfd3981eb86995cf377d60a6bde008e
Andrew Ivanov <106903166+F1ameX@users.noreply.github.com>
2025-11-15 13:36:56 +0700

    SOCKS Proxy initial commit

---
+++ THIRD_GRADE/FIRST_SEMESTER/Networks/lab_5/State.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class State(Enum):
+    GREETING = 0
+    CONNECTION_REQUEST = 1
+    RESOLVING = 2
---
+++ THIRD_GRADE/FIRST_SEMESTER/Networks/lab_5/config.py
@@ -0,0 +1,19 @@
+class Config:
+    def __init__(self, host='127.0.0.1', port=1080):
+        self.host = host
+        self.port = port
+
+    @classmethod
+    def from_dict(cls, config_dict):
+        host = config_dict.get('host', '127.0.0.1')
+        port = config_dict.get('port', 1080)
+        return cls(host, port)
+
+    def to_dict(self):
+        return {
+            'host': self.host,
+            'port': self.port
+        }
+
+    def __str__(self):
+        return f'Config(host={self.host}, port={self.port})'
